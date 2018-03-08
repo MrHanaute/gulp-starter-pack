44f5f40dd4faad6f4340ca37ccaab1a152342bd6
William M. da Silva <william@grupoartisan.com.br>
2018-03-08 18:17:53 +0000

    add bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -72,7 +72,6 @@ gulp.task('default', function (callback) {
 
 gulp.task('watch', ['browserSync', 'sass'], function (){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
-  // Reloads the browser whenever HTML or JS files change
   gulp.watch('app/*.html', browserSync.reload); 
   gulp.watch('app/js/**/*.js', browserSync.reload); 
 });
